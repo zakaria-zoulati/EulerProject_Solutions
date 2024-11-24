e11cfb146e746a5f106c97a4eb5448175abb7337
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-11-24 23:56:14 +0100

    done

--- Problem 12/solution.py
+++ Problem 12/solution.py
@@ -6,18 +6,21 @@ def divs(n):
     a = int(math.sqrt(n))
     for i in range(1,  a+ 1):
         if n % i == 0:
-            count += 2  # Count both i and n/i
+            count += 2  
     if a*a == n : 
         count -= 1 
         
     return count
 
+def main()  : 
+    t = 500
+    curr = 28
+    add  = 8  
+    while( divs( curr ) <= t  ) :
+        curr += add 
+        add += 1 
+    print(curr) 
 
-t = 500
-curr = 28
-add  = 8  
-while( divs( curr ) <= t  ) :
-    curr += add 
-    add += 1 
-
-print( curr )
+startTime = time.time() 
+main() 
+print( time.time() - startTime )
