2c1ef82891f311159bbd59284799a6d464015799
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-11-24 15:11:19 +0100

    done

---
+++ Problem 7/solution.py
@@ -0,0 +1,23 @@
+from math import  sqrt
+
+n = 10001
+
+def isPrime(n) : 
+    a = int(sqrt(n)) 
+    for i in range( 2,a+1 ) : 
+        if n % i == 0 : 
+            return False 
+    return True
+
+curr = -1 
+i = 2
+while n > 0 :
+    if isPrime(i) : 
+        curr = i 
+        n -= 1
+    i += 1 
+
+print(curr)
+
+
+
