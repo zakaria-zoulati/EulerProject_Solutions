177d503a85574f9469c131b82fbc45374f6a3dd2
Zakaria Zoulati <zoulatizakaria3@gmail.com>
2024-11-25 21:59:52 +0100

    done

---
+++ Problem 13/solution.java
@@ -0,0 +1,20 @@
+import java.util.* ; 
+import java.io.File ; 
+import java.io.FileNotFoundException ; 
+import java.math.BigInteger;
+public class solution {
+    public static void main( String[] args ){
+        try {
+            File f = new File("in.txt") ;
+            Scanner sc = new Scanner(f) ;  
+            BigInteger bg = new BigInteger("0") ; 
+            while( sc.hasNextLine() ){
+                bg = bg.add( new BigInteger(sc.nextLine() )) ; 
+            }
+            System.out.println( bg.toString() ) ; 
+        }catch (FileNotFoundException e) {
+            System.out.println("Zzzzz ..") ; 
+        }
+        
+    }
+}
