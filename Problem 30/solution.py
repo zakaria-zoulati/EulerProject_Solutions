34d9d31a0f3638ea6fe4eb57a58c3a9b49d5e21a
zakaria-zoulati <zoulatiazakaria3@gmail.com>
2025-01-24 00:31:04 +0100

    Problem 20 Solution

---
+++ Problem 30/solution.py
@@ -0,0 +1,19 @@
+rs = 0 
+limit = 1000000
+
+
+arr = [ i**5 for i in range(0,10) ]
+
+def isValid(n) :
+    inter = 0 
+    num = n 
+    while( n > 0 ) : 
+        inter += arr[ int(n%10) ]
+        n /= 10
+    return num == inter 
+
+for i in range( 2 , limit ):
+    if isValid(i):
+        rs += i 
+
+print(rs)
