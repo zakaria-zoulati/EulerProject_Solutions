7be20f1949a8a7504e1b376a2a1b1355aa1df645
zakaria-zoulati <zoulatizakaria3@gmail.com>
2025-01-24 19:45:11 +0100

    Final Solution

--- Problem 25/solution.py
+++ Problem 25/solution.py
@@ -8,27 +8,14 @@ def find_first_fibonacci_with_k_digits(k):
     left, right = 1, 10000  
     while left < right:
         mid = left + (right - left) // 2
-        if fibonacci_digits(mid+1) < k:  
+        if fibonacci_digits(mid) < k:  
             left = mid + 1
         else:  
             right = mid
     return left
 
 k = 1000 
-result = find_first_fibonacci_with_k_digits(k) + 1 
+result = find_first_fibonacci_with_k_digits(k) 
 print(f"The first Fibonacci number with {k} digits is F_{result}")
 
-# def fib(n) : 
-#     if n==1 or n == 2 :
-#         return 1
-#     return fib(n-1) + fib(n-2) 
-
-# arr = [ 0, 1 ]
-# for i in range( 3 , 1000 ):
-#     n = len( arr ) 
-#     a = arr[n-1] + arr[n-2] 
-#     if( a % 10 == 0 ) :
-#         print(i) 
-#         print(a)
-#     arr.append( a )
 
